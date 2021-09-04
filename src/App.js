0a2fe0890e58af9aedfb15c4833c941919bc5905
tomas.mencner <tomas.mencner@embedit.cz>
2021-09-04 12:00:28 +0200

    working solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+function generatePassword(length) {
+  var result = "";
+  var specialChars = "!#$%&()*+,-.:;<=>?@[]^_{|}~";
+  var upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+  var numerics = "0123456789";
+  var characters = lowerAlphabet + upperAlphabet + numerics + specialChars;
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function copyToClipboard() {
+  var input = document.getElementById("passId");
+  var text = input.value;
+  input.select();
+  if (window.clipboardData && window.clipboardData.setData) {
+    return window.clipboardData.setData("Text", text);
+  } else if (
+    document.queryCommandSupported &&
+    document.queryCommandSupported("copy")
+  ) {
+    try {
+      return document.execCommand("copy");
+    } catch (ex) {
+      console.warn("Copy to clipboard failed.", ex);
+      return false;
+    }
+  }
+}
 
 function App() {
+  const [password, setPassword] = useState(null);
+
+  useEffect(() => {
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var len = url.searchParams.get("len");
+    console.log(len);
+    if (len) {
+      setPassword(generatePassword(len));
+    } else {
+      setPassword(generatePassword(20));
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" id="passId" value={password} />
+      <button
+        onClick={() => {
+          copyToClipboard(password);
+        }}
+      >
+        Copy to clipboard
+      </button>
     </div>
   );
 }
